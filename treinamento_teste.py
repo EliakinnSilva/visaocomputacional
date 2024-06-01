3b883d11f947f83792d7eb809f85a653bcb35598
Eliakinn Enoque <112765893+EliakinnSilva@users.noreply.github.com>
2024-06-01 10:27:46 -0300

    Atualizando o script do treinamento

---
+++ treinamento_teste.py
@@ -0,0 +1,53 @@
+import os
+import numpy as np
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import ModelCheckpoint
+
+# Caminho para o conjunto de dados
+dataset_path = 'dataset/'
+
+# Configurações do gerador de dados
+datagen = ImageDataGenerator(validation_split=0.2, rescale=1./255, 
+                             rotation_range=20, width_shift_range=0.2, height_shift_range=0.2,
+                             shear_range=0.2, zoom_range=0.2, horizontal_flip=True, fill_mode='nearest')
+
+train_generator = datagen.flow_from_directory(
+    dataset_path,
+    target_size=(224, 224),
+    batch_size=32,
+    class_mode='categorical',
+    subset='training'
+)
+
+validation_generator = datagen.flow_from_directory(
+    dataset_path,
+    target_size=(224, 224),
+    batch_size=32,
+    class_mode='categorical',
+    subset='validation'
+)
+
+# Criação do modelo
+num_classes = len(train_generator.class_indices)
+base_model = MobileNetV2(weights='imagenet', include_top=False)
+model = Sequential([
+    base_model,
+    GlobalAveragePooling2D(),
+    Dense(num_classes, activation='softmax')
+])
+
+# Compilação do modelo
+model.compile(optimizer=Adam(), loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Callbacks
+checkpoint = ModelCheckpoint('user_recognition_model.keras', monitor='val_loss', save_best_only=True, verbose=1)
+
+# Treinamento do modelo
+model.fit(train_generator, epochs=10, validation_data=validation_generator, callbacks=[checkpoint])
+
+# Salvar o modelo treinado
+model.save('user_recognition_model_final.keras')
